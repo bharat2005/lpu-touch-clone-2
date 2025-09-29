ba2c54040320dd850b0a4be9d00cc9cad2da87d7
bharat2005 <bharatvdeshm2005@gmail.com>
2025-09-29 14:24:44 +0530

    Forestail and Winray aded

--- app/src/main/java/com/example/lputouch/data/source/MyColorsList.kt
+++ app/src/main/java/com/example/lputouch/data/source/MyColorsList.kt
@@ -3,11 +3,13 @@ package com.example.lputouch.data.source
 import com.example.lputouch.data.model.MyColor
 import com.example.lputouch.ui.theme.BlueChip
 import com.example.lputouch.ui.theme.ElegantPurple
+import com.example.lputouch.ui.theme.Forestial
 import com.example.lputouch.ui.theme.FourLeaf
 import com.example.lputouch.ui.theme.Noir
 import com.example.lputouch.ui.theme.Pool
 import com.example.lputouch.ui.theme.Purple
 import com.example.lputouch.ui.theme.Vienna
+import com.example.lputouch.ui.theme.Wineary
 
 val myColorsList : List<MyColor> = listOf(
     MyColor(FourLeaf, "FourLeaf"),
@@ -16,6 +18,8 @@ val myColorsList : List<MyColor> = listOf(
     MyColor(Noir, "Noir"),
     MyColor(Vienna, "Vienna"),
     MyColor(Purple, "Purple"),
-    MyColor(Pool, "Pool")
+    MyColor(Pool, "Pool"),
+    MyColor(Forestial, "Forestial"),
+    MyColor(Wineary, "Wineary")
 
 )
